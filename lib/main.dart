c447dba08fe9407b984a8cd438dcd5ff87a56e95
Md Rahmat Ullah <mdrahmatullahmaruf@gmail.com>
2022-01-06 00:09:05 -0800

    added style and decoration

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 import 'transaction.dart';
 
@@ -20,7 +18,7 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   final List<Transaction> transactions = [
     Transaction(
-        id: 't1', title: "New Shoes", amount: 65.5, date: DateTime.now()),
+        id: 't1', title: "New Shoes", amount: 65.65, date: DateTime.now()),
     Transaction(
         id: 't2', title: "Monitor", amount: 79.86, date: DateTime.now()),
   ];
@@ -55,20 +53,31 @@ class MyHomePage extends StatelessWidget {
                         ),
                         decoration: BoxDecoration(
                           border: Border.all(
-                            color: Colors.black,
+                            color: Colors.purple,
                             width: 2,
                           ),
                         ),
                         padding: EdgeInsets.all(10),
                         child: Text(
                           "\$" + e.amount.toString(),
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Colors.purple),
                         ),
                       ),
                       Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Text("Title: " + e.title),
-                          Text("Date: " + e.date.toString()),
+                          Text(
+                            "Title: " + e.title,
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 16),
+                          ),
+                          Text(
+                            "Date: " + e.date.toString(),
+                            style: TextStyle(color: Colors.grey),
+                          ),
                         ],
                       ),
                     ],
