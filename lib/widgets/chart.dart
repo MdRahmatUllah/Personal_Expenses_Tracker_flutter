40643014792dda5c8324f7a0ff2a30c45619dffe
Md Rahmat Ullah <mdrahmatullahmaruf@gmail.com>
2022-01-14 22:13:14 -0800

    108 reversed list and set fix height

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -27,7 +27,7 @@ class Chart extends StatelessWidget {
         'day': DateFormat.E().format(weekDay).substring(0, 1),
         'amount': totalAmount
       };
-    });
+    }).reversed.toList();
   }
 
   double get totalSpending {
