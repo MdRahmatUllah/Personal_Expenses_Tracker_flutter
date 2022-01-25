9b3850b798e54e8060f133ece88b54e010f8f715
Md Rahmat Ullah <mdrahmatullahmaruf@gmail.com>
2022-01-26 02:16:05 +0600

    130 - 136 adding iOS design

---
+++ lib/widgets/adaptive_flat_button.dart
@@ -0,0 +1,27 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class AdaptiveFlatButton extends StatelessWidget {
+  Function handler;
+  final String text;
+  AdaptiveFlatButton(this.text, this.handler);
+  @override
+  Widget build(BuildContext context) {
+    return Platform.isIOS
+        ? CupertinoButton(
+            child: Text(
+              text,
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            onPressed: handler,
+          )
+        : FlatButton(
+            textColor: Theme.of(context).primaryColor,
+            onPressed: handler,
+            child: Text(
+              text,
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ));
+  }
+}
